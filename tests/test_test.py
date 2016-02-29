91e94b2fac49a444a9b506e11723057f03cc472f
Falk Schuetzenmeister <falk.schuetzenmeister@tnc.org>
2016-02-29 13:24:31 -0800

    remove invalid package reference

--- tests/test_test.py
+++ tests/test_test.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from sqlite3 import dbapi2 as db
 
 
 class TestNothing(TestCase):
@@ -6,3 +7,10 @@ class TestNothing(TestCase):
     def test_nothing(self):
         silly = 'hello world'
         self.assertEqual(silly, 'hello world')
+
+
+class TestLibSpatialite(TestCase):
+
+    def test_database(self):
+        conn = db.connect('test.sqlite')
+        cur = conn.cursor()
